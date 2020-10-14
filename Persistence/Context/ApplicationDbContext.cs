4b686f0f59bb34630ec2bfe091ec960fa886f299
yonatan <yonimethodic@gmail.com>
2020-10-14 17:24:09 +0300

    Add project files.

---
+++ Persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+public class ApplicationDbContext : DbContext, IApplicationDbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+    public DbSet<Product> Products { get; set; }
+    public async Task<int> SaveChanges()
+    {
+        return await base.SaveChangesAsync();
+    }
+
+    public Task SaveChangesAsync()
+    {
+        return  base.SaveChangesAsync();
+    }
+}
---
+++ Persistence/DependencyInjection.cs
@@ -0,0 +1,16 @@
+﻿using Application.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static void AddPersistence(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(
+                configuration.GetConnectionString("DefaultConnection"),
+                b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+        services.AddScoped<IApplicationDbContext>(provider => provider.GetService<ApplicationDbContext>());
+    }
+}
